ec701bbb2aecece8e1a812101d8def90dd91eb24
raymond zhao <raymondzhao158@gmail.com>
2021-04-26 11:08:56 -0700

    commit tree 5be7a241c3efb7c01166fb3d14a8f643f974fc1b

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import MouseComponent from './MouseComponent';
+import GraceHopperQuoteComponent from './GraceHopperQuoteComponent'
+import CatComponent from './CatComponent'
 
 class App extends Component {
 	render() {
@@ -7,7 +10,7 @@ class App extends Component {
 			<div className="App">
 				<CatComponent />
 				<GraceHopperQuoteComponent />
-				{/* one more component missing */}
+				<MouseComponent />
 			</div>
 		);
 	}
